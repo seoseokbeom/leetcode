f4a391d130f3840c86ef3be4b727bb41d8fc3d85
unknown <pronunciatio@naver.com>
2020-06-25 05:35:25 +0900

    16 3sum closest [finished]

--- leetcode16_3sumClosest.js
+++ leetcode16_3sumClosest.js
@@ -5,37 +5,27 @@
 var threeSumClosest = function(nums, target) {
     sortArr=nums.sort(function(a, b){return a - b});
     var arr=[];
-    console.log(sortArr);
+    var ans = sortArr[0]+sortArr[1]+sortArr[nums.length-1];
+    var smallestGap = Math.abs(sortArr[0]+sortArr[1]+sortArr[nums.length-1] -target);
     for(var i=0; i<nums.length-2; i++){
-        // if (sortArr[i] > target) break;
         if (i > 0 && nums[i] == nums[i - 1]) {              // skip same result
             continue;
         }
         var j=i+1;
-        // if (sortArr[i] === sortArr[i + 1]) continue;
         var k=nums.length-1;
-        var ans = sortArr[0]+sortArr[1]+sortArr[2];
-        var smallestGap = Math.abs(sortArr[i]+sortArr[j]+sortArr[k] -target);
         while(i<j && j<k) {
-            console.log(smallestGap);
             var l=arr.length;
-            // if(l!=0 && arr[l-1][0]==sortArr[i] && arr[l-1][1]==sortArr[j]) {
-            //     j++;
-            //     continue;
-            // }
             if(Math.abs(sortArr[i]+sortArr[j]+sortArr[k] - target) < smallestGap ) {
                 smallestGap = Math.abs(sortArr[i]+sortArr[j]+sortArr[k] - target);
                 ans = sortArr[i]+sortArr[j]+sortArr[k];
-                j++;
             }
-            else if(sortArr[i]+sortArr[j]+sortArr[k]<target) j++;
-            else if(sortArr[i]+sortArr[j]+sortArr[k]>target) k--;
-            else j++;
+            if(sortArr[i]+sortArr[j]+sortArr[k]<=target) j++;
+            if(sortArr[i]+sortArr[j]+sortArr[k]>target) k--;
         }
     }
     return ans;
 };
-console.log(threeSumClosest([0,2,1,-3],1));
+console.log(threeSumClosest([-1,2,1,-4], 1));
 // console.log(threeSumClosest([-1,2,1,-4], 1));
 // console.log(threeSumClosest([-1,2,4,2,2,3,1,3,1,-4], 23));
 // console.log(threeSumClosest([-1,2,4,2,2,3,1,3,1,-4], -23));
