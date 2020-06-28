b50ad26ca6464b55fb09f0721cba81b720375ef6
unknown <pronunciatio@naver.com>
2020-06-29 06:01:05 +0900

    17. Letter Combinations of a Phone Number[unfinished]

--- leetcode17LetterCombination.js
+++ leetcode17LetterCombination.js
@@ -3,26 +3,50 @@
  * @return {string[]}
  */
 
-var numToChar = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+var map = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+
+// var print = (a,b) {
+
+// }
+
 
 var recursive = function(digits) {
     if(digits.length==0) return "";
     // var str = 
-    console.log(digits[0]);
-    console.log(parseInt(digits[0]));
-    console.log(numToChar[parseInt(digits[0])]);
-    var  a = numToChar[parseInt(digits[0])].length;
-    for(var i=0; i<a; i++) {
-        return numToChar[digits[0]][i]+recursive(digits.slice(1));
+    // console.log(digits[0]);
+    // console.log(parseInt(digits[0]));
+    let digitsNum = parseInt(digits);
+
+    // console.log(digits);
+    // console.log(numToString[parseInt(digits[0])]);
+    var arr = [];
+    for(let i=0;i<digits.length;i++) {
+        arr.push()
     }
+    // var a = numToString[parseInt(digits[0])].length;
+    // var b = numToString[parseInt(digits[0])].length;
+    // for(var i=0; i<a; i++) {
+    //     return numToString[digits[0]][i]+recursive(digits.slice(1));
+    // }
 }
 
 var letterCombinations = function(digits) {
-    var array = [];
-    array.push(recursive(digits));
-    return array;
+    let res = [];
+    function go(i,s) {
+        if(i === digits.length) {
+            res.push(s);
+            return;
+        }
+
+        for(let c of map[digits[i]]) {
+            go(i+1, s+c);
+        }
+    }
+
+    go(0, '');
+    return res;
 };
 
-console.log(recursive("23"));
+console.log(letterCombinations("23"));
 // var tmp = "123";
 // console.log(tmp[0]);
