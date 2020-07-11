ceac3d79414bdd5fab17e6c7fca082e0088b24d3
unknown <pronunciatio@naver.com>
2020-07-11 22:02:58 +0900

    39 Combination Sum [finished]

--- leetcode39CombinationSum.js
+++ leetcode39CombinationSum.js
@@ -3,66 +3,29 @@
  * @param {number} target
  * @return {number[][]}
  */
-
- /**
- * @param {number[]} candidates
- * @param {number} target
- * @return {number[][]}
- */
 var combinationSum = function(candidates, target) {
-    var buffer= [];
-    var result= [];
-    var recursive= (index, target) => {
-        if(target ==0 ) return result.push(buffer.slice());
-        if(target < 0) 
-        {
+    var res =[];
+    var recursion =(sum, arr, idx) => {
+        if(sum>target) return;
+        if(sum==target){
+            // console.log(arr);
+            res.push([...arr]);
+            // console.log(res);
             return;
         }
-        if(index == candidates.length) return;
-        buffer.push(candidates[index]);
-        recursive(index, target-candidates[index]);
-        buffer.pop();
-        recursive(index+1, target);
-    }           
-    
-    recursive(0,target);
-    return result;
+        if(idx==candidates.length) return;
+        // console.log(idx);
+        arr.push(candidates[idx]);
+        recursion(sum+candidates[idx], arr, idx);
+        arr.pop();
+        recursion(sum, arr, idx+1);
+        // for(var elem of candidates) {
+        //     arr.push(elem);
+        //     // console.log(sum+elem, arr);
+        //     recursion(sum+elem, arr);
+        //     arr.pop();
+        // }
+    }
+    recursion(0,[],0);
+    return res;
 };
-
-console.log(combinationSum([2,3,6,7],7));
-
-// var combinationSum = function(candidates, target) {
-//     candidates.sort((a,b)=> a-b);
-//     for(let i=0; i<candidates.length; i++) { 
-//         if(candidates[i] > target) candidates.splice(i,1);
-//     }
-//     let sum=0;
-//     arr2d=[];
-//     let recersive = (arr, i, sum, target) => {
-//         if(sum+candidates[i]>target) {
-//             console.log('canceled');
-//             return;}
-//         if(sum+candidates[i]==target) {
-//             console.log("--matched sum--",sum+candidates[i]);
-//             arr.push(candidates[i]);
-//             arr2d.push(arr);
-//             console.log("arr2d",arr2d);
-//             return;
-//         }
-//         if(sum+candidates[i]<target) {
-//             arr.push(candidates[i]);
-//             sum=sum+candidates[i];
-//             console.log("sum",sum, "added num:",candidates[i]);
-//             for(let j=0; j<candidates.length; j++) {
-//                 recersive(arr, j, sum, target);
-//             }
-//         }
-//         return;
-//     }
-//     for(let i=0; i<candidates.length; i++){
-//         recersive([], i, 0, target);
-//     }
-//     return arr2d;
-// //     recersive(arr2d, arr, i, sum, target) {
-// //     }
-// };
