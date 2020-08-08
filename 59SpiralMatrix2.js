43c564e4c57423b0a6e45d89c92a808a6fa3d306
unknown <pronunciatio@naver.com>
2020-08-09 08:00:10 +0900

    59 Spiral Matrix 2 [updated, finished]

--- 59SpiralMatrix2.js
+++ 59SpiralMatrix2.js
@@ -4,34 +4,23 @@
  */
 var generateMatrix = function(n) {
     let matrix = Array(n).fill().map(()=>Array(n).fill(Number.MAX_VALUE));
-    // console.log(matrix);
     let i=0;
     let j=0;
     let count=1;
     matrix[0][0]=count;
     while(count<n*n){
-        console.log('5');
         while(j<n-1 && count < matrix[i][j+1] ) {
-            console.log('1');
-            ++j;
-            matrix[i][j]=++count;
+            matrix[i][++j]=++count;
         }
         while(i<n-1 && count < matrix[i+1][j]) {
-            console.log('2');
-            ++i;
-            matrix[i][j]=++count;
+            matrix[++i][j]=++count;
         }
         while(j>0 && count < matrix[i][j-1]) {
-            console.log('3');
-            --j;
-            matrix[i][j]=++count;
+            matrix[i][--j]=++count;
         }
         while(i>0 && count < matrix[i-1][j]) {
-            console.log('4');
-            --i;
-            matrix[i][j]=++count;
+            matrix[--i][j]=++count;
         }
-        console.log('count',count);
     }
     return matrix;
 };
