44cec3e9776930a16fe8d684213f946b9b852708
unknown <pronunciatio@naver.com>
2020-09-02 11:10:43 +0900

    1528 Shuffle string [f]

---
+++ 1528ShuffleString.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def restoreString(self, s, indices):
+        ans = ['']*len(s)
+        for i in range(len(s)):
+            ans[indices[i]] = s[i]
+        return ''.join(ans)
+
+
+a = Solution()
+print(a.restoreString(s="aiohn", indices=[3, 1, 4, 2, 0]))
