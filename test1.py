5f5c007a6599013158891f9f436e226b7356f27c
unknown <pronunciatio@naver.com>
2020-12-09 01:26:35 +0900

    1678. Goal Parser Interpretation

--- test1.py
+++ test1.py
@@ -1,10 +1,5 @@
-import sys
 
-
-tmp = [1, 2, 3]+[0]*999999
-# print(len(tmp))
-# for i in range(3, 900000):
-#     tmp[i] = tmp[i-1]+tmp[i-2]
-
-# print(tmp[0:100])
-print(sys.getsizeof(tmp))
+a = [1, 2, 3, 4, 7]
+b = [2, 3, 5, 55, 3]
+print(max(a+b))
+print(a+b)
