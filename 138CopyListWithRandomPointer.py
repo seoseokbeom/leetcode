a692cf69db4a37e5718846132c14e633a49d8661
unknown <pronunciatio@naver.com>
2020-12-14 23:52:26 +0900

    138. Copy List with Random Pointer [f]

--- 138CopyListWithRandomPointer.py
+++ 138CopyListWithRandomPointer.py
@@ -8,7 +8,20 @@ class Node:
 """
 
 
-class Solution(object):
-    def copyRandomList(self, head):
-      a = Node(head.val, Node(head.next.val), Node(head.random))
-      
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if head is None:
+            return None
+        mapping = {}
+        cur = head
+        while cur:
+            mapping[cur] = Node(cur.val, None, None)
+            cur = cur.next
+        cur = head
+        while cur:
+            if cur.next:
+                mapping[cur].next = mapping[cur.next]
+            if cur.random:
+                mapping[cur].random = mapping[cur.random]
+            cur = cur.next
+        return mapping[head]
